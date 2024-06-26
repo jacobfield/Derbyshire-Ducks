c5895053e527de73540422cb51aaebb5191d0041
Jake Field <jacobprogrammes@gmail.com>
2024-06-26 12:39:33 +0100

    logo imputted

--- derbyshire-ducks/src/comps/Header.js
+++ derbyshire-ducks/src/comps/Header.js
@@ -1,10 +1,16 @@
-import ducksLogo from "./public/ducksLogo.png";
+import ducksLogo from "../assets/ducksLogo.png";
 
 export default function Header() {
   return (
     <>
       <div className="headerDiv">
         <h1 className="headerTitle">Derbyshire Ducks</h1>
+        <p>Contact</p>
+        <img
+          className="ducksLogo"
+          alt="Derbyshire Ducks Logo"
+          src={ducksLogo}
+        />
       </div>
     </>
   );
