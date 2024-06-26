c7a4785a21d488347460396c0372a001955a51bb
Jake Field <jacobprogrammes@gmail.com>
2024-06-26 12:28:25 +0100

    image import

--- derbyshire-ducks/src/App.js
+++ derbyshire-ducks/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Header from "./comps/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="App-header">
+        <Header></Header>
+      </div>
     </div>
   );
 }
--- derbyshire-ducks/src/comps/Header.js
+++ derbyshire-ducks/src/comps/Header.js
@@ -1,1 +1,11 @@
-export default function Header() {}
+import ducksLogo from "./public/ducksLogo.png";
+
+export default function Header() {
+  return (
+    <>
+      <div className="headerDiv">
+        <h1 className="headerTitle">Derbyshire Ducks</h1>
+      </div>
+    </>
+  );
+}
