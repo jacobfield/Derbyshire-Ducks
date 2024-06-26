338cf1f2beb9f0802e314b861c6437841b60d2d0
Jake Field <jacobprogrammes@gmail.com>
2024-06-26 12:41:01 +0100

    logo positioning

--- derbyshire-ducks/src/comps/Header.js
+++ derbyshire-ducks/src/comps/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <>
       <div className="headerDiv">
         <h1 className="headerTitle">Derbyshire Ducks</h1>
-        <p>Contact</p>
+        {/* <p>Contact</p> */}
         <img
           className="ducksLogo"
           alt="Derbyshire Ducks Logo"
