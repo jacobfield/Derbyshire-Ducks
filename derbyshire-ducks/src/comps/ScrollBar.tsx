edd628b6e862b1b4a90006f10063b3a31e02a0fc
Jake Field <jacobprogrammes@gmail.com>
2024-06-27 12:59:13 +0100

    MovingBar comp added

---
+++ derbyshire-ducks/src/comps/ScrollBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovingBar: React.FC = () => {
+  return (
+    <div className="movingBar">
+      <p className="movingText"> This text is moving</p>
+    </div>
+  );
+};
+
+export default MovingBar;
