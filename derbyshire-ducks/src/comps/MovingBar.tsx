0e9e5ff940a63aad9fe827a00d8f375d4e19df8f
Jake Field <jacobprogrammes@gmail.com>
2024-06-28 14:47:28 +0100

    update #s

--- derbyshire-ducks/src/comps/MovingBar.tsx
+++ derbyshire-ducks/src/comps/MovingBar.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const MovingBar: React.FC = () => {
   return (
     <div className="movingBar">
-      <p className="movingText"> This text is moving</p>
+      <p className="movingText">
+        #DerbyshireDucks #JoinTheFlock #QuackQuack #FlagFootball #BAFA
+        #QuackAttack #DuckSquad #VFormation #FlagIsSwag #Takeoff
+      </p>
     </div>
   );
 };
