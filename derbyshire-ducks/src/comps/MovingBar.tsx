f523346ed457c0b381dd7b43d5d54831825814e3
Jake Field <jacobprogrammes@gmail.com>
2024-06-28 15:08:15 +0100

    Seamless transition. now want it to loop more consistently

--- derbyshire-ducks/src/comps/MovingBar.tsx
+++ derbyshire-ducks/src/comps/MovingBar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 const MovingBar: React.FC = () => {
   const [position, setPosition] = useState(window.innerWidth);
+  const textRef = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     const moveText = () => {
@@ -10,20 +11,38 @@ const MovingBar: React.FC = () => {
     };
     const interval = setInterval(moveText, 20); // can adjust for smoother animation
 
-    if (position < -2000) {
+    if (position < -1570) {
       setPosition(window.innerWidth);
     }
     return () => clearInterval(interval); //cleanup on comp unmount
   }, [position]);
 
+  useEffect(() => {
+    const updatePositionBasedOnTextWidth = () => {
+      if (textRef.current) {
+        const textWidth = textRef.current.offsetWidth; //measure text width
+        console.log(`text width: ${textWidth}px`);
+
+        if (position < -textWidth) {
+          setPosition(window.innerWidth);
+        }
+      }
+    };
+    window.addEventListener(`resize`, updatePositionBasedOnTextWidth);
+    updatePositionBasedOnTextWidth();
+    return () =>
+      window.removeEventListener(`resize`, updatePositionBasedOnTextWidth);
+  }, [position]);
+
   return (
     <div className="movingBar">
       <p
+        ref={textRef}
         className="movingText"
         style={{ transform: `translateX(${position}px` }}
       >
         #DerbyshireDucks #JoinTheFlock #QuackQuack #FlagFootball #BAFA
-        #QuackAttack #DuckSquad #VFormation #FlagIsSwag #Takeoff
+        #QuackAttack #DuckSquad #FlyingVFormation #FlagIsSwag #Takeoff
       </p>
     </div>
   );
