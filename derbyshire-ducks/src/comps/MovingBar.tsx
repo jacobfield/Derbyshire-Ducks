9fb9cc86fa2c043c775819d5dff0853317b73aaa
Jake Field <jacobprogrammes@gmail.com>
2024-06-28 14:59:22 +0100

    converted to js

--- derbyshire-ducks/src/comps/MovingBar.tsx
+++ derbyshire-ducks/src/comps/MovingBar.tsx
@@ -6,12 +6,22 @@ const MovingBar: React.FC = () => {
   useEffect(() => {
     const moveText = () => {
       // update position to move left.
+      setPosition((prevPosition) => prevPosition - 2);
     };
-  }, []);
+    const interval = setInterval(moveText, 20); // can adjust for smoother animation
+
+    if (position < -2000) {
+      setPosition(window.innerWidth);
+    }
+    return () => clearInterval(interval); //cleanup on comp unmount
+  }, [position]);
 
   return (
     <div className="movingBar">
-      <p className="movingText">
+      <p
+        className="movingText"
+        style={{ transform: `translateX(${position}px` }}
+      >
         #DerbyshireDucks #JoinTheFlock #QuackQuack #FlagFootball #BAFA
         #QuackAttack #DuckSquad #VFormation #FlagIsSwag #Takeoff
       </p>
