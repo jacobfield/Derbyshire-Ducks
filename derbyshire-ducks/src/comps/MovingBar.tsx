97fd434233b12a90d6100942acd7e2869944d18c
Jake Field <jacobprogrammes@gmail.com>
2024-06-28 14:54:26 +0100

    converting the css to Js

--- derbyshire-ducks/src/comps/MovingBar.tsx
+++ derbyshire-ducks/src/comps/MovingBar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 const MovingBar: React.FC = () => {
+  const [position, setPosition] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const moveText = () => {
+      // update position to move left.
+    };
+  }, []);
+
   return (
     <div className="movingBar">
       <p className="movingText">
