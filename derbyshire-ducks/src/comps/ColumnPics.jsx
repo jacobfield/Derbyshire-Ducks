c66b2885fe3755cadf3a79bf285d3710ee9c04e0
Jake Field <jacobprogrammes@gmail.com>
2024-07-30 11:01:07 +0100

    new comp for column pics

---
+++ derbyshire-ducks/src/comps/ColumnPics.jsx
@@ -0,0 +1,10 @@
+import column-1 from 'derbyshire-ducks/src/assets/ducks_pics/column-1.png';
+
+
+export default function ColumnPics() {
+  return (
+    <div className="picsContainer">
+      <img alt="ducks team column" src="column-1.png"></img>
+    </div>
+  );
+}
