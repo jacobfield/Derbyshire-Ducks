5c04619390dce3ce45096ea643461eec8e08ea9e
Jake Field <jacobprogrammes@gmail.com>
2024-07-30 15:24:26 +0100

    4 column pics added and formatted

--- derbyshire-ducks/src/comps/ColumnPics.jsx
+++ derbyshire-ducks/src/comps/ColumnPics.jsx
@@ -1,24 +1,29 @@
 import column1 from "../assets/ducks_pics/column1.png";
 import column2 from "../assets/ducks_pics/column2.png";
 import column3 from "../assets/ducks_pics/column3.jpg";
-
+import column4 from "../assets/ducks_pics/column4.png";
 export default function ColumnPics() {
   return (
     <div className="picsContainer">
       <img
         className="columnPic pic1"
-        alt="ducks team column"
+        alt="ducks team column 1"
         src={column1}
       ></img>
       <img
         className="columnPic pic2"
-        alt="ducks team column"
+        alt="ducks team column 2 "
         src={column2}
       ></img>
       <img
         className="columnPic pic3"
-        alt="ducks team column"
+        alt="ducks team column 3"
         src={column3}
+      ></img>
+      <img
+        className="columnPic pic4"
+        alt="ducks team column 4"
+        src={column4}
       ></img>
     </div>
   );
