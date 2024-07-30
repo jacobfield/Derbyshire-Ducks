5fd1154669b277d1ef7300d419448dbdadadfac0
Jake Field <jacobprogrammes@gmail.com>
2024-07-30 11:03:30 +0100

    column pic comp complete

--- derbyshire-ducks/src/comps/ColumnPics.jsx
+++ derbyshire-ducks/src/comps/ColumnPics.jsx
@@ -1,10 +1,25 @@
-import column-1 from 'derbyshire-ducks/src/assets/ducks_pics/column-1.png';
-
+import column1 from "derbyshire-ducks/src/assets/ducks_pics/column1.png";
+import column2 from "derbyshire-ducks/src/assets/ducks_pics/column2.png";
+import column3 from "derbyshire-ducks/src/assets/ducks_pics/column3.png";
 
 export default function ColumnPics() {
   return (
     <div className="picsContainer">
-      <img alt="ducks team column" src="column-1.png"></img>
+      <img
+        className="columnPic pic1"
+        alt="ducks team column"
+        src={column1}
+      ></img>
+      <img
+        className="columnPic pic2"
+        alt="ducks team column"
+        src={column2}
+      ></img>
+      <img
+        className="columnPic pic3"
+        alt="ducks team column"
+        src={column3}
+      ></img>
     </div>
   );
 }
