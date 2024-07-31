8fe429a2b8459ea0d3a8c7281c70de6d0ef2e614
Jake Field <jacobprogrammes@gmail.com>
2024-07-31 10:36:49 +0100

    display grid added, media queries updated, behind text added

--- derbyshire-ducks/src/comps/ColumnPics.jsx
+++ derbyshire-ducks/src/comps/ColumnPics.jsx
@@ -6,31 +6,46 @@ import teamPic from "../assets/ducks_pics/teamPic.jpg";
 export default function ColumnPics() {
   return (
     <div className="picsContainer">
-      <img
-        className="columnPic pic1"
-        alt="ducks team column 1"
-        src={column1}
-      ></img>
-      <img
-        className="columnPic pic2"
-        alt="ducks team column 2"
-        src={column2}
-      ></img>
-      <img
-        className="columnPic pic3"
-        alt="ducks team column 3"
-        src={column3}
-      ></img>
-      <img
-        className="columnPic pic4"
-        alt="ducks team column 4"
-        src={column4}
-      ></img>{" "}
-      <img
-        className="teamPic pic5"
-        alt="Derbyshire Ducks Team"
-        src={teamPic}
-      ></img>
+      <div className="imageWrapper">
+        <img
+          className="columnPic pic1"
+          alt="ducks team column 1"
+          src={column1}
+        ></img>{" "}
+        <div className="overlayText">Column 1</div>
+      </div>
+      <div className="imageWrapper">
+        <img
+          className="columnPic pic2"
+          alt="ducks team column 2"
+          src={column2}
+        ></img>
+        <div className="overlayText">Column 2</div>
+      </div>
+      <div className="imageWrapper">
+        <img
+          className="columnPic pic3"
+          alt="ducks team column 3"
+          src={column3}
+        ></img>
+        <div className="overlayText">Column 3</div>
+      </div>
+      <div className="imageWrapper">
+        <img
+          className="columnPic pic4"
+          alt="ducks team column 4"
+          src={column4}
+        ></img>
+        <div className="overlayText">Column 4</div>
+      </div>
+      <div className="imageWrapper">
+        <img
+          className="teamPic pic5"
+          alt="Derbyshire Ducks Team"
+          src={teamPic}
+        ></img>
+        <div className="overlayText">Derbyshire Ducks Team</div>
+      </div>
     </div>
   );
 }
